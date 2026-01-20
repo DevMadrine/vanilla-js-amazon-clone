705a17590b261141553e7a4b738e0e5a594a15b1
DevMadrine <madrinenamulindwa90@gmail.com>
2026-01-20 11:04:54 +0300

    Added images and separated data from the javascript implementation to product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,38 +1,3 @@
-const products = [
-  {
-
-    image: 'breakfast',
-    name: 'Breakfast',
-    rating:{
-      stars: 4.5,
-      count: 87
-    },
-    price: 1095
-  },
-    {
-
-    image: 'breakfast',
-    name: 'Lunch',
-    rating:{
-      stars: 4.0,
-      count: 180
-    },
-    price: 1000
-  },
-
-    {
-
-    image: 'breakfast',
-    name: 'Drinks',
-    rating:{
-      stars: 5.0,
-      count: 59
-    },
-    price: 8709
-  }
-
-];
-
 let productHTML =  '';
 
 products.forEach((product) =>{
@@ -40,38 +5,38 @@ products.forEach((product) =>{
 
   <div class="product-container">
     <div class="product-image-container">
-      <img src="./images/${product.image}.jpg" alt="breakfast">
+      <img class="product-image" src="${product.image}">
     </div>
-    <div class="product-name">${product.name}</div>
-    <div class="product-rating-container">
-      <img src="${product.rating.stars}.jpg" alt="" class="product-rating-stars">
+  <div class="product-name">${product.name}</div>
+       <div class="product-rating-container">
+       <img src="images/star-${product.rating.stars*10}.png" alt="" class="product-rating-stars">
       <div class="product-rating-count">${product.rating.count}</div>
-    </div>
-    <div class="product-price">
-      $${(product.price/100).toFixed(2)}
-    </div>
-    <div class="product-quantity">
-      <select>
-        <option selected value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
+  </div>
+  <div class="product-price">
+    $${(product.price/100).toFixed(2)}
+  </div>
+  <div class="product-quantity">
+  <select class="quantity-select">
+   <option selected value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
         <option value="9">9</option>
         <option value="10">10</option>
-      </select>
-    </div>
-    <div class="product-spacer"></div>
-    <div class="added-to-cart">
-      <img src="">
-    Added
-    </div>
-  <button class="add-to-cart">
-    Add to Cart
-  </button>
+  </select>
+  </div>
+ <div class="product-spacer"></div>
+ <div class="added-to-cart">
+<img src="">
+  
+  </div>
+ <button class="add-to-cart">
+ Add to Cart
+ </button>
   </div>
   `
 });
---
+++ scripts/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+
+    image: 'images/stocks.jpeg',
+    name: 'Men stockings Black and white',
+    rating:{
+      stars: 4.5,
+      count: 87
+    },
+    price: 1095
+  },
+    {
+
+    image: 'images/basketball.jpeg',
+    name: 'Intermediate Size Basketball',
+    rating:{
+      stars: 4.0,
+      count: 180
+    },
+    price: 1000
+  },
+
+    {
+
+    image: 'images/shirts.jpeg',
+    name: 'Men cotton shirts 3 pieces',
+    rating:{
+      stars: 3.5,
+      count: 59
+    },
+    price: 8709
+  }
+
+];
